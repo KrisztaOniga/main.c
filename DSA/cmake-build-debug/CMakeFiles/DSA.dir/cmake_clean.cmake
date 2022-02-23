43ee80af2ba11d1879600bdf541238854e8f4a1c
KrisztaOniga <99740799+KrisztaOniga@users.noreply.github.com>
2022-02-23 13:10:12 +0200

    DSA

---
+++ DSA/cmake-build-debug/CMakeFiles/DSA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA.dir/main.c.obj"
+  "DSA.exe"
+  "DSA.exe.manifest"
+  "DSA.pdb"
+  "libDSA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
